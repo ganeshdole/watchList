b7bce504f6a5667b72ef483c6320df6f44c10423
ganeshdole <dole77770@gmail.com>
2024-02-25 12:43:25 +0530

    Done with fetching movies data

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const movieInput = document.getElementById("movie-input");
 const searchBtn = document.getElementById("search-btn");
+const apiKey = "65c98c14";
+let moviesInfo = [];
+let searchResult = [];
 
-searchBtn.addEventListener("click", () => {
-  fetch(`http://www.omdbapi.com/?apikey=65c98c14&s=${movieInput.value}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      document.getElementById("search-results").innerHTML = data.Search.map(
-        (movie) => {
-          return `<div class="movie>
-               <h3>${movie.Title}</h3>
-          </div>
-          `;
-        }
-      ).join(" ");
-      console.log(data);
-    });
+async function getMovieInfo() {
+  moviesInfo = [];
+  for (const movie of searchResult) {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`
+    );
+    const data = await response.json();
+    moviesInfo.push(data);
+  }
+  render();
+}
+
+searchBtn.addEventListener("click", async () => {
+  const response = await fetch(
+    `http://www.omdbapi.com/?apikey=${apiKey}&s=${movieInput.value}`
+  );
+  const result = await response.json();
+  searchResult = result.Search;
+  getMovieInfo();
 });
+
+function renderMovies() {
+  return moviesInfo
+    .map((movie) => {
+      return `
+    <div class="movie">
+        <img class="movie-poster" src="${movie.Poster}"/>
+        <h1>${movie.Title}</h1>
+        <p>${movie.Ratings[0].Value.slice(0, 3)}⭐️</p>
+        <p>${movie.Runtime}</p>
+        <p>${movie.Genre}</p>
+        <p>${movie.Plot}</p>
+    </div>
+    `;
+    })
+    .join(" ");
+}
+
+function render() {
+  document.getElementById("search-results").innerHTML = renderMovies();
+}
